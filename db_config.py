8e1c29a08f73bc66b27ebbd3426adc9edb17f302
Anupam Das <163879128+anupamdas55@users.noreply.github.com>
2025-11-27 23:01:50 +0530

    Refactor environment variable loading in db_config

--- db_config.py
+++ db_config.py
@@ -1,14 +1,41 @@
+# import os
+# import psycopg2
+# from dotenv import load_dotenv
+# # Load .env file
+# load_dotenv()
+
+# DB_HOST = os.getenv("DB_HOST")
+# DB_NAME = os.getenv("DB_NAME")
+# DB_USER = os.getenv("DB_USER")
+# DB_PASS = os.getenv("DB_PASS")
+# DB_PORT = os.getenv("DB_PORT", 5432)
+
+# def get_connection():
+#     return psycopg2.connect(
+#         host=DB_HOST,
+#         dbname=DB_NAME,
+#         user=DB_USER,
+#         password=DB_PASS,
+#         port=DB_PORT,
+#         sslmode="require"
+#     )
+
+# if __name__ == "__main__":
+#     try:
+#         conn = get_connection()
+#         print("✅ Database connection successful!")
+#         conn.close()
+#     except Exception as e:
+#         print("❌ Connection failed:", e)
+
 import os
 import psycopg2
-from dotenv import load_dotenv
-# Load .env file
-load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_PORT = os.getenv("DB_PORT", 5432)
+DB_HOST = os.environ.get("DB_HOST")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+DB_PORT = os.environ.get("DB_PORT", 5432)
 
 def get_connection():
     return psycopg2.connect(
@@ -20,10 +47,4 @@ def get_connection():
         sslmode="require"
     )
 
-if __name__ == "__main__":
-    try:
-        conn = get_connection()
-        print("✅ Database connection successful!")
-        conn.close()
-    except Exception as e:
-        print("❌ Connection failed:", e)
+
