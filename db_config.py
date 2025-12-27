af3b1bedb5f7324208fe28969b0fc61d0ce907ed
Anupam Das <163879128+anupamdas55@users.noreply.github.com>
2025-12-27 12:38:58 +0530

    Clean up db_config.py by removing comments

--- db_config.py
+++ db_config.py
@@ -1,33 +1,3 @@
-# import os
-# import psycopg2
-# from dotenv import load_dotenv
-# # Load .env file
-# load_dotenv()
-
-# DB_HOST = os.getenv("DB_HOST")
-# DB_NAME = os.getenv("DB_NAME")
-# DB_USER = os.getenv("DB_USER")
-# DB_PASS = os.getenv("DB_PASS")
-# DB_PORT = os.getenv("DB_PORT", 5432)
-
-# def get_connection():
-#     return psycopg2.connect(
-#         host=DB_HOST,
-#         dbname=DB_NAME,
-#         user=DB_USER,
-#         password=DB_PASS,
-#         port=DB_PORT,
-#         sslmode="require"
-#     )
-
-# if __name__ == "__main__":
-#     try:
-#         conn = get_connection()
-#         print("✅ Database connection successful!")
-#         conn.close()
-#     except Exception as e:
-#         print("❌ Connection failed:", e)
-
 import os
 import psycopg2
 
@@ -46,5 +16,6 @@ def get_connection():
         port=DB_PORT,
         sslmode="require"
     )
+
 
 
